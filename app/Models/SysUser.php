005206f53c4f34228aa863f84a6745f318e5e4e2
Jabez <jabezandrew.aguspina.22@usjr.edu.ph>
2025-07-02 12:43:33 +0800

    Sanctum implementation

--- app/Models/SysUser.php
+++ app/Models/SysUser.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace App\Models;
-
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class SysUser extends Authenticatable
 {
-
-
+    use HasApiTokens, HasFactory, Notifiable;
+    
     public $timestamps = false;
     
     protected $fillable = [
